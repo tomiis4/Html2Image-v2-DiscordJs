0eede7e7ab3d5c357093e03bc65a12c76b53592e
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-08-10 21:25:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const { REST  } = require('@discordjs/rest');
-const  { Client, GatewayIntentBits, AttachmentBuilder, EmbedBuilder, Routes, ActivityType  } = require('discord.js');
+const  { Client, GatewayIntentBits, AttachmentBuilder, EmbedBuilder, Routes, ActionRowBuilder, SelectMenuBuilder, ActivityType  } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -16,8 +16,6 @@ const randomColor = Math.floor(Math.random()*16777215).toString(16);
 const Buffer = require('buffer').Buffer;
 
 client.on("ready", () => {
-    client.user.setActivity("New version out!", { type: ActivityType.Watching });
-
     console.log(`[DISCORD] ${client.user.username} has been online`);
 })
 
@@ -40,6 +38,29 @@ const commands = [
             }
         ]
     },
+    {
+        name:  'learn',
+        description: 'You can learn HTML & CSS there',
+        type: 1,
+        options: [
+            {
+                name: "language",
+                description: "Select language you want learn-",
+                type: 3,
+                required: true,
+                choices: [
+                    {
+                        name: "html",
+                        value: "Learn HTML"
+                    },
+                    {
+                        name: "css",
+                        value: "Learn CSS"
+                    }
+                ]
+            }
+        ]
+    }
 ];
 
 
@@ -50,8 +71,8 @@ const commands = [
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
-
-		console.log('Successfully reloaded application (/) commands.');
+	
+        console.log('Successfully reloaded application (/) commands.');
 	} catch (error) {
 		console.error(error);
 	}
@@ -59,11 +80,13 @@ const commands = [
 
 
 
-// Reply
-client.on('interactionCreate',  interaction => {
+// Reply on run messages
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
     let htmlInteraction = interaction.options._hoistedOptions[0]
     let cssInteraction = interaction.options._hoistedOptions[1]
-    if (interaction.commandName === "run") { // if run command "RUN"
+    if (interaction.commandName == "run") { // if command "RUN"
         // if html and css
         if (htmlInteraction != undefined && cssInteraction != undefined) { 
             const dataSend = {
@@ -80,12 +103,11 @@ client.on('interactionCreate',  interaction => {
                         .setImage('attachment://CodeResult.png')
                         .setColor(`#${randomColor}`)
                         .setTimestamp()
-                    
                     interaction.reply({ embeds: [codeEmbed], files: [image64Attachment] })
                 })
         }
         // if html
-        else if (htmlInteraction != undefined && cssInteraction == undefined) {
+        else if (htmlInteraction != undefined) {
             const dataSend = {
                 "html": htmlInteraction.value
             }
@@ -99,7 +121,6 @@ client.on('interactionCreate',  interaction => {
                         .setImage('attachment://CodeResult.png')
                         .setColor(`#${randomColor}`)
                         .setTimestamp()
-                    
                     interaction.reply({ embeds: [codeEmbed], files: [image64Attachment] })
                 })
         }
