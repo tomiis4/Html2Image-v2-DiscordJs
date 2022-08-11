2f1ea712f4db131c704be076c10d78bd97a9e1cb
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-08-11 20:13:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const client = new Client({
     ]
 })
 
-const clientId = process.env.ID;
+const config = require("./config.json");
+const clientId = config.clientID;
 const guildId = '941415311519916033';
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(config.token);
 const randomColor = Math.floor(Math.random()*16777215).toString(16);
 const Buffer = require('buffer').Buffer;
 
@@ -92,7 +93,7 @@ client.on('interactionCreate', async interaction => {
                 "html": htmlInteraction.value,
                 "css": cssInteraction.value
             }
-            axios.post('https://html2image-v2-api.herokuapp.com//api', dataSend) // get data from API
+            axios.post('http://localhost:3000/api', dataSend) // get data from API
                 .then((res) => {
                     const image64 = Buffer.from(res.data, 'base64');
                     const image64Attachment = new AttachmentBuilder(image64, { name: "CodeResult.png"});
@@ -110,7 +111,7 @@ client.on('interactionCreate', async interaction => {
             const dataSend = {
                 "html": htmlInteraction.value
             }
-            axios.post('https://html2image-v2-api.herokuapp.com//api', dataSend) // get data from API
+            axios.post('http://localhost:3000/api', dataSend) // get data from API
                 .then((res) => {
                     const image64 = Buffer.from(res.data, 'base64');
                     const image64Attachment = new AttachmentBuilder(image64, { name: "CodeResult.png"});
@@ -130,5 +131,56 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
+// Select  learn messages
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
 
-client.login(process.env.TOKEN);
+    let optionsInteraction = interaction.options._hoistedOptions[0].value
+    if (interaction.commandName == "learn") { // if  command learn
+        // if learn html
+        if (optionsInteraction == "Learn HTML") {
+            const selectMenuHtml = new ActionRowBuilder() // create menu for help html
+                .addComponents(
+                    new SelectMenuBuilder()
+                        .setCustomId('learnHtml')
+                        .setPlaceholder('Nothing is selected')
+                        .addOptions(
+                            {
+                                label: 'Introduction',
+                                description: 'Introduction in to the HTML',
+                                value: 'html-introduction',
+                            },
+                        ),
+                );
+            await interaction.reply({ components: [selectMenuHtml] });
+        } 
+        // if learn css
+        else if (optionsInteraction  == "Learn CSS") {
+            interaction.reply("Comming soon.")
+        }        
+        // wrong answer  (never happend)
+        else { 
+            interaction.reply("Wrong format.");
+        }
+    }
+});
+client.on("interactionCreate", interaction =>{
+    if(interaction.values != undefined) {
+        const interactionName = interaction.values[0]
+
+        switch(interactionName) { // check what component is selected
+            case "html-introduction":
+                const htmlEmbed = new EmbedBuilder() // create embed
+                    .setTitle("Introduction")
+                    .setDescription("• HTML stand for *Hyper Text Markup Language*\n• Describes the structure of a Web page.\n• Most used version is HTML5")
+                    .setTimestamp()
+                    .setColor("#e85417")
+                    .setImage('https://i.imgur.com/AfFp7pu.png')
+                interaction.reply({ embeds: [htmlEmbed] })
+                break;
+        }
+    }
+})
+
+
+client.login(config.token);
